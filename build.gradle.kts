78a1148beae9a4bf45e15cde5acfccaf6f2150b8
Pavel Vashkel <jgugn1@gmail.com>
2023-12-16 00:32:36 +0100

    chore: configure detekt + detekt compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,17 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.detekt) apply true
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.asProvider().get()
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+}
+
+dependencies {
+    detektPlugins(libs.detekt.compose)
 }
 
 true // Needed to make the Suppress annotation work for the plugins block

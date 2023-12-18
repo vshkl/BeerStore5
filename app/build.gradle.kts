8545d7b288c1cd44b89111e0dbd98ac6c48639c9
Pavel Vashkel <jgugn1@gmail.com>
2023-12-18 05:09:53 +0100

    chore: add view model dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ sqldelight {
 dependencies {
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
